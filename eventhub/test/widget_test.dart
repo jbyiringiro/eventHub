223941048891f14ea8c115fc50865e01b170494a
MAHE Digne <156647199+dmahe09@users.noreply.github.com>
2025-11-26 20:26:55 +0200

    Add initial widget test for Counter increments

--- eventhub/test/widget_test.dart
+++ eventhub/test/widget_test.dart
@@ -1,3 +1,4 @@
+
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
@@ -11,6 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:eventhub/main.dart';
 
 void main() {
+  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const EventHubApp());
